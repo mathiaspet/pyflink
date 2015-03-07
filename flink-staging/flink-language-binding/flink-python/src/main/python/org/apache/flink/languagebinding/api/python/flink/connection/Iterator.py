3b5afaf5939f9e0e4190d69519aefb46cf1d4889
zentol <s.motsu@web.de>
2015-03-07 08:51:43 +0100

    extend fix to byte[]

--- flink-staging/flink-language-binding/flink-python/src/main/python/org/apache/flink/languagebinding/api/python/flink/connection/Iterator.py
+++ flink-staging/flink-language-binding/flink-python/src/main/python/org/apache/flink/languagebinding/api/python/flink/connection/Iterator.py
@@ -260,7 +260,7 @@ class ByteArrayDeserializer(object):
 
     def deserialize(self):
         size = unpack(">i", self.read(4, self._group))[0]
-        return bytearray(self.read(size, self._group))
+        return bytearray(self.read(size, self._group)) if size else bytearray(b"")
 
 
 class BooleanDeserializer(object):
