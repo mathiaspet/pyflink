apache/flink

[FLINK-2160] Change Streaming Source Interface to run(Context)/cancel()
The context can be used to emit elements and retrieve the checkpointing
lock object.

In the future, the context can be extended to provide support for
element emission with timestamps and dealing with the watermark system.

flink-staging/flink-streaming/flink-streaming-connectors/flink-connector-kafka/src/main/java/org/apache/flink/streaming/connectors/kafka/api/KafkaSource.java
@@ -32,7 +32,6 @@ import kafka.javaapi.consumer.ConsumerConnector;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.source.ConnectorSource;
 import org.apache.flink.streaming.util.serialization.DeserializationSchema;
-import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -186,7 +185,7 @@ public class KafkaSource<OUT> extends ConnectorSource<OUT> {
 	}
 
 	@Override
-	public void run(Object checkpointLock, Collector<OUT> collector) throws Exception {
+	public void run(SourceContext<OUT> ctx) throws Exception {
 		
 		// NOTE: Since this source is not checkpointed, we do not need to
 		// acquire the checkpoint lock
@@ -196,7 +195,7 @@ public class KafkaSource<OUT> extends ConnectorSource<OUT> {
 				if (schema.isEndOfStream(out)) {
 					break;
 				}
-				collector.collect(out);
+				ctx.emit(out);
 			}
 		} finally {
 			consumer.shutdown();

flink-staging/flink-streaming/flink-streaming-connectors/flink-connector-kafka/src/test/java/org/apache/flink/streaming/connectors/kafka/KafkaITCase.java
@@ -399,13 +399,14 @@ public class KafkaITCase {
 			boolean running = true;
 
 			@Override
-			public void run(Object checkpointLock, Collector<Tuple2<Integer, Integer>> collector) throws Exception {
+			public void run(SourceContext<Tuple2<Integer, Integer>> ctx) throws Exception {
 				LOG.info("Starting source.");
 				int cnt = from;
 				int partition = getRuntimeContext().getIndexOfThisSubtask();
 				while (running) {
 					LOG.info("Writing " + cnt + " to partition " + partition);
-					collector.collect(new Tuple2<Integer, Integer>(getRuntimeContext().getIndexOfThisSubtask(), cnt));
+					ctx.emit(new Tuple2<Integer, Integer>(getRuntimeContext().getIndexOfThisSubtask(),
+							cnt));
 					if (cnt == to) {
 						LOG.info("Writer reached end.");
 						return;
@@ -492,11 +493,11 @@ public class KafkaITCase {
 			boolean running = true;
 
 			@Override
-			public void run(Object checkpointLock, Collector<Tuple2<Long, String>> collector) throws Exception {
+			public void run(SourceContext<Tuple2<Long, String>> ctx) throws Exception {
 				LOG.info("Starting source.");
 				int cnt = 0;
 				while (running) {
-					collector.collect(new Tuple2<Long, String>(1000L + cnt, "kafka-" + cnt++));
+					ctx.emit(new Tuple2<Long, String>(1000L + cnt, "kafka-" + cnt++));
 					try {
 						Thread.sleep(100);
 					} catch (InterruptedException ignored) {
@@ -576,11 +577,11 @@ public class KafkaITCase {
 			boolean running = true;
 
 			@Override
-			public void run(Object checkpointLock, Collector<Tuple2<Long, String>> collector) throws Exception {
+			public void run(SourceContext<Tuple2<Long, String>> ctx) throws Exception {
 				LOG.info("Starting source.");
 				int cnt = 0;
 				while (running) {
-					collector.collect(new Tuple2<Long, String>(1000L + cnt, "kafka-" + cnt++));
+					ctx.emit(new Tuple2<Long, String>(1000L + cnt, "kafka-" + cnt++));
 					LOG.info("Produced " + cnt);
 
 					try {
@@ -668,14 +669,14 @@ public class KafkaITCase {
 			}
 
 			@Override
-			public void run(Object checkpointLock, Collector<Tuple2<Long, byte[]>> collector) throws Exception {
+			public void run(SourceContext<Tuple2<Long, byte[]>> ctx) throws Exception {
 				LOG.info("Starting source.");
 				long cnt = 0;
 				Random rnd = new Random(1337);
 				while (running) {
 					//
 					byte[] wl = new byte[Math.abs(rnd.nextInt(1024 * 1024 * 30))];
-					collector.collect(new Tuple2<Long, byte[]>(cnt++, wl));
+					ctx.emit(new Tuple2<Long, byte[]>(cnt++, wl));
 					LOG.info("Emitted cnt=" + (cnt - 1) + " with byte.length = " + wl.length);
 
 					try {
@@ -685,7 +686,7 @@ public class KafkaITCase {
 					if(cnt == 10) {
 						LOG.info("Send end signal");
 						// signal end
-						collector.collect(new Tuple2<Long, byte[]>(-1L, new byte[]{1}));
+						ctx.emit(new Tuple2<Long, byte[]>(-1L, new byte[]{1}));
 						running = false;
 					}
 				}
@@ -776,11 +777,11 @@ public class KafkaITCase {
 			boolean running = true;
 
 			@Override
-			public void run(Object checkpointLock, Collector<Tuple2<Long, String>> collector) throws Exception {
+			public void run(SourceContext<Tuple2<Long, String>> ctx) throws Exception {
 				LOG.info("Starting source.");
 				int cnt = 0;
 				while (running) {
-					collector.collect(new Tuple2<Long, String>(1000L + cnt, "kafka-" + cnt++));
+					ctx.emit(new Tuple2<Long, String>(1000L + cnt, "kafka-" + cnt++));
 					try {
 						Thread.sleep(100);
 					} catch (InterruptedException ignored) {
@@ -869,11 +870,11 @@ public class KafkaITCase {
 			boolean running = true;
 
 			@Override
-			public void run(Object checkpointLock, Collector<String> collector) throws Exception {
+			public void run(SourceContext<String> ctx) throws Exception {
 				LOG.info("Starting source.");
 				int cnt = 0;
 				while (running) {
-					collector.collect("kafka-" + cnt++);
+					ctx.emit("kafka-" + cnt++);
 					try {
 						Thread.sleep(100);
 					} catch (InterruptedException ignored) {
@@ -911,12 +912,12 @@ public class KafkaITCase {
 			boolean running = true;
 
 			@Override
-			public void run(Object checkpointLock, Collector<String> collector) throws Exception {
+			public void run(SourceContext<String> ctx) throws Exception {
 				LOG.info("Starting source.");
 				int cnt = 0;
 				while (running) {
 					String msg = "kafka-" + cnt++;
-					collector.collect(msg);
+					ctx.emit(msg);
 					LOG.info("sending message = "+msg);
 
 					if ((cnt - 1) % 20 == 0) {

flink-staging/flink-streaming/flink-streaming-connectors/flink-connector-rabbitmq/src/main/java/org/apache/flink/streaming/connectors/rabbitmq/RMQSource.java
@@ -27,7 +27,6 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.QueueingConsumer;
-import org.apache.flink.util.Collector;
 
 public class RMQSource<OUT> extends ConnectorSource<OUT> {
 	private static final long serialVersionUID = 1L;
@@ -86,7 +85,7 @@ public class RMQSource<OUT> extends ConnectorSource<OUT> {
 	}
 
 	@Override
-	public void run(Object checkpointLock, Collector<OUT> out) throws Exception {
+	public void run(SourceContext<OUT> ctx) throws Exception {
 		while (running) {
 			delivery = consumer.nextDelivery();
 
@@ -95,7 +94,7 @@ public class RMQSource<OUT> extends ConnectorSource<OUT> {
 				break;
 			}
 
-			out.collect(result);
+			ctx.emit(result);
 		}
 	}
 

flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/functions/source/FromElementsFunction.java
@@ -21,8 +21,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 
-import org.apache.flink.util.Collector;
-
 public class FromElementsFunction<T> implements SourceFunction<T> {
 	
 	private static final long serialVersionUID = 1L;
@@ -44,11 +42,11 @@ public class FromElementsFunction<T> implements SourceFunction<T> {
 	}
 
 	@Override
-	public void run(Object checkpointLock, Collector<T> out) throws Exception {
+	public void run(SourceContext<T> ctx) throws Exception {
 		Iterator<T> it = iterable.iterator();
 
 		while (isRunning && it.hasNext()) {
-			out.collect(it.next());
+			ctx.emit(it.next());
 		}
 	}
 

flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/functions/source/FromIteratorFunction.java
@@ -17,8 +17,6 @@
 
 package org.apache.flink.streaming.api.functions.source;
 
-import org.apache.flink.util.Collector;
-
 import java.util.Iterator;
 
 public class FromIteratorFunction<T> implements SourceFunction<T> {
@@ -34,9 +32,9 @@ public class FromIteratorFunction<T> implements SourceFunction<T> {
 	}
 
 	@Override
-	public void run(Object checkpointLock, Collector<T> out) throws Exception {
+	public void run(SourceContext<T> ctx) throws Exception {
 		while (isRunning && iterator.hasNext()) {
-			out.collect(iterator.next());
+			ctx.emit(iterator.next());
 		}
 	}
 

flink-staging/flink-streaming/flink-streaming-core/src/test/java/org/apache/flink/streaming/api/complex/ComplexIntegrationTest.java
@@ -484,11 +484,10 @@ public class ComplexIntegrationTest extends StreamingMultipleProgramsTestBase {
 		long cnt = 0;
 
 		@Override
-		public void run(Object checkpointLock,
-				Collector<OuterPojo> out) throws Exception {
+		public void run(SourceContext<OuterPojo> ctx) throws Exception {
 			for (int i = 0; i < 20; i++) {
 				OuterPojo result = new OuterPojo(new InnerPojo(cnt / 2, "water_melon-b"), 2L);
-				out.collect(result);
+				ctx.emit(result);
 			}
 		}
 
@@ -502,11 +501,10 @@ public class ComplexIntegrationTest extends StreamingMultipleProgramsTestBase {
 		private static final long serialVersionUID = 1L;
 
 		@Override
-		public void run(Object checkpointLock,
-				Collector<Tuple2<Long, Tuple2<String, Long>>> out) throws Exception {
+		public void run(SourceContext<Tuple2<Long, Tuple2<String, Long>>> ctx) throws Exception {
 			for (int i = 0; i < 20; i++) {
 				Tuple2<Long, Tuple2<String, Long>> result = new Tuple2<Long, Tuple2<String, Long>>(1L, new Tuple2<String, Long>("a", 1L));
-				out.collect(result);
+				ctx.emit(result);
 			}
 		}
 
@@ -622,10 +620,9 @@ public class ComplexIntegrationTest extends StreamingMultipleProgramsTestBase {
 		}
 
 		@Override
-		public void run(Object checkpointLock,
-				Collector<Rectangle> out) throws Exception {
+		public void run(SourceContext<Rectangle> ctx) throws Exception {
 			for (int i = 0; i < 100; i++) {
-				out.collect(rectangle);
+				ctx.emit(rectangle);
 				rectangle = rectangle.next();
 			}
 		}

flink-staging/flink-streaming/flink-streaming-examples/src/main/java/org/apache/flink/streaming/examples/windowing/TopSpeedWindowingExample.java
@@ -26,7 +26,6 @@ import org.apache.flink.streaming.api.windowing.deltafunction.DeltaFunction;
 import org.apache.flink.streaming.api.windowing.helper.Delta;
 import org.apache.flink.streaming.api.windowing.helper.Time;
 import org.apache.flink.streaming.api.windowing.helper.Timestamp;
-import org.apache.flink.util.Collector;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -108,8 +107,7 @@ public class TopSpeedWindowingExample {
 		}
 
 		@Override
-		public void run(Object checkpointLock, Collector<Tuple4<Integer, Integer, Double, Long>> collector)
-				throws Exception {
+		public void run(SourceContext<Tuple4<Integer, Integer, Double, Long>> ctx) throws Exception {
 
 			while (isRunning) {
 				Thread.sleep(1000);
@@ -122,7 +120,7 @@ public class TopSpeedWindowingExample {
 					distances[carId] += speeds[carId] / 3.6d;
 					Tuple4<Integer, Integer, Double, Long> record = new Tuple4<Integer, Integer, Double, Long>(carId,
 							speeds[carId], distances[carId], System.currentTimeMillis());
-					collector.collect(record);
+					ctx.emit(record);
 				}
 			}
 		}

flink-tests/src/test/java/org/apache/flink/test/checkpointing/StreamCheckpointingITCase.java
@@ -35,7 +35,6 @@ import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
 import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
 import org.apache.flink.test.util.ForkableFlinkMiniCluster;
 
-import org.apache.flink.util.Collector;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -219,7 +218,9 @@ public class StreamCheckpointingITCase {
 		}
 
 		@Override
-		public void run(Object lockingObject, Collector out) throws Exception {
+		public void run(SourceContext<String> ctx) throws Exception {
+			final Object lockingObject = ctx.getCheckpointLock();
+
 			while (isRunning && index < numElements) {
 				char first = (char) ((index % 40) + 40);
 
@@ -230,7 +231,7 @@ public class StreamCheckpointingITCase {
 
 				synchronized (lockingObject) {
 					index += step;
-					out.collect(result);
+					ctx.emit(result);
 				}
 			}
 		}

mathiaspet/pyflink

flink-staging/flink-streaming/flink-streaming-connectors/flink-connector-kafka/src/main/java/org/apache/flink/streaming/connectors/kafka/api/KafkaSource.java
@@ -32,7 +32,6 @@ import kafka.javaapi.consumer.ConsumerConnector;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.source.ConnectorSource;
 import org.apache.flink.streaming.util.serialization.DeserializationSchema;
-import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -186,7 +185,7 @@ public class KafkaSource<OUT> extends ConnectorSource<OUT> {
 	}
 
 	@Override
-	public void run(Object checkpointLock, Collector<OUT> collector) throws Exception {
+	public void run(SourceContext<OUT> ctx) throws Exception {
 		
 		// NOTE: Since this source is not checkpointed, we do not need to
 		// acquire the checkpoint lock
@@ -196,7 +195,7 @@ public class KafkaSource<OUT> extends ConnectorSource<OUT> {
 				if (schema.isEndOfStream(out)) {
 					break;
 				}
-				collector.collect(out);
+				ctx.collect(out);
 			}
 		} finally {
 			consumer.shutdown();

flink-staging/flink-streaming/flink-streaming-connectors/flink-connector-kafka/src/test/java/org/apache/flink/streaming/connectors/kafka/KafkaITCase.java
@@ -399,13 +399,14 @@ public class KafkaITCase {
 			boolean running = true;
 
 			@Override
-			public void run(Object checkpointLock, Collector<Tuple2<Integer, Integer>> collector) throws Exception {
+			public void run(SourceContext<Tuple2<Integer, Integer>> ctx) throws Exception {
 				LOG.info("Starting source.");
 				int cnt = from;
 				int partition = getRuntimeContext().getIndexOfThisSubtask();
 				while (running) {
 					LOG.info("Writing " + cnt + " to partition " + partition);
-					collector.collect(new Tuple2<Integer, Integer>(getRuntimeContext().getIndexOfThisSubtask(), cnt));
+					ctx.collect(new Tuple2<Integer, Integer>(getRuntimeContext().getIndexOfThisSubtask(),
+							cnt));
 					if (cnt == to) {
 						LOG.info("Writer reached end.");
 						return;
@@ -492,11 +493,11 @@ public class KafkaITCase {
 			boolean running = true;
 
 			@Override
-			public void run(Object checkpointLock, Collector<Tuple2<Long, String>> collector) throws Exception {
+			public void run(SourceContext<Tuple2<Long, String>> ctx) throws Exception {
 				LOG.info("Starting source.");
 				int cnt = 0;
 				while (running) {
-					collector.collect(new Tuple2<Long, String>(1000L + cnt, "kafka-" + cnt++));
+					ctx.collect(new Tuple2<Long, String>(1000L + cnt, "kafka-" + cnt++));
 					try {
 						Thread.sleep(100);
 					} catch (InterruptedException ignored) {
@@ -576,11 +577,11 @@ public class KafkaITCase {
 			boolean running = true;
 
 			@Override
-			public void run(Object checkpointLock, Collector<Tuple2<Long, String>> collector) throws Exception {
+			public void run(SourceContext<Tuple2<Long, String>> ctx) throws Exception {
 				LOG.info("Starting source.");
 				int cnt = 0;
 				while (running) {
-					collector.collect(new Tuple2<Long, String>(1000L + cnt, "kafka-" + cnt++));
+					ctx.collect(new Tuple2<Long, String>(1000L + cnt, "kafka-" + cnt++));
 					LOG.info("Produced " + cnt);
 
 					try {
@@ -668,14 +669,14 @@ public class KafkaITCase {
 			}
 
 			@Override
-			public void run(Object checkpointLock, Collector<Tuple2<Long, byte[]>> collector) throws Exception {
+			public void run(SourceContext<Tuple2<Long, byte[]>> ctx) throws Exception {
 				LOG.info("Starting source.");
 				long cnt = 0;
 				Random rnd = new Random(1337);
 				while (running) {
 					//
 					byte[] wl = new byte[Math.abs(rnd.nextInt(1024 * 1024 * 30))];
-					collector.collect(new Tuple2<Long, byte[]>(cnt++, wl));
+					ctx.collect(new Tuple2<Long, byte[]>(cnt++, wl));
 					LOG.info("Emitted cnt=" + (cnt - 1) + " with byte.length = " + wl.length);
 
 					try {
@@ -685,7 +686,7 @@ public class KafkaITCase {
 					if(cnt == 10) {
 						LOG.info("Send end signal");
 						// signal end
-						collector.collect(new Tuple2<Long, byte[]>(-1L, new byte[]{1}));
+						ctx.collect(new Tuple2<Long, byte[]>(-1L, new byte[]{1}));
 						running = false;
 					}
 				}
@@ -776,11 +777,11 @@ public class KafkaITCase {
 			boolean running = true;
 
 			@Override
-			public void run(Object checkpointLock, Collector<Tuple2<Long, String>> collector) throws Exception {
+			public void run(SourceContext<Tuple2<Long, String>> ctx) throws Exception {
 				LOG.info("Starting source.");
 				int cnt = 0;
 				while (running) {
-					collector.collect(new Tuple2<Long, String>(1000L + cnt, "kafka-" + cnt++));
+					ctx.collect(new Tuple2<Long, String>(1000L + cnt, "kafka-" + cnt++));
 					try {
 						Thread.sleep(100);
 					} catch (InterruptedException ignored) {
@@ -869,11 +870,11 @@ public class KafkaITCase {
 			boolean running = true;
 
 			@Override
-			public void run(Object checkpointLock, Collector<String> collector) throws Exception {
+			public void run(SourceContext<String> ctx) throws Exception {
 				LOG.info("Starting source.");
 				int cnt = 0;
 				while (running) {
-					collector.collect("kafka-" + cnt++);
+					ctx.collect("kafka-" + cnt++);
 					try {
 						Thread.sleep(100);
 					} catch (InterruptedException ignored) {
@@ -911,12 +912,12 @@ public class KafkaITCase {
 			boolean running = true;
 
 			@Override
-			public void run(Object checkpointLock, Collector<String> collector) throws Exception {
+			public void run(SourceContext<String> ctx) throws Exception {
 				LOG.info("Starting source.");
 				int cnt = 0;
 				while (running) {
 					String msg = "kafka-" + cnt++;
-					collector.collect(msg);
+					ctx.collect(msg);
 					LOG.info("sending message = "+msg);
 
 					if ((cnt - 1) % 20 == 0) {

flink-staging/flink-streaming/flink-streaming-connectors/flink-connector-rabbitmq/src/main/java/org/apache/flink/streaming/connectors/rabbitmq/RMQSource.java
@@ -27,7 +27,6 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.QueueingConsumer;
-import org.apache.flink.util.Collector;
 
 public class RMQSource<OUT> extends ConnectorSource<OUT> {
 	private static final long serialVersionUID = 1L;
@@ -86,7 +85,7 @@ public class RMQSource<OUT> extends ConnectorSource<OUT> {
 	}
 
 	@Override
-	public void run(Object checkpointLock, Collector<OUT> out) throws Exception {
+	public void run(SourceContext<OUT> ctx) throws Exception {
 		while (running) {
 			delivery = consumer.nextDelivery();
 
@@ -95,7 +94,7 @@ public class RMQSource<OUT> extends ConnectorSource<OUT> {
 				break;
 			}
 
-			out.collect(result);
+			ctx.collect(result);
 		}
 	}
 

flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/functions/source/FromElementsFunction.java
@@ -21,8 +21,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 
-import org.apache.flink.util.Collector;
-
 public class FromElementsFunction<T> implements SourceFunction<T> {
 	
 	private static final long serialVersionUID = 1L;
@@ -44,11 +42,11 @@ public class FromElementsFunction<T> implements SourceFunction<T> {
 	}
 
 	@Override
-	public void run(Object checkpointLock, Collector<T> out) throws Exception {
+	public void run(SourceContext<T> ctx) throws Exception {
 		Iterator<T> it = iterable.iterator();
 
 		while (isRunning && it.hasNext()) {
-			out.collect(it.next());
+			ctx.collect(it.next());
 		}
 	}
 

flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/functions/source/FromIteratorFunction.java
@@ -17,8 +17,6 @@
 
 package org.apache.flink.streaming.api.functions.source;
 
-import org.apache.flink.util.Collector;
-
 import java.util.Iterator;
 
 public class FromIteratorFunction<T> implements SourceFunction<T> {
@@ -34,9 +32,9 @@ public class FromIteratorFunction<T> implements SourceFunction<T> {
 	}
 
 	@Override
-	public void run(Object checkpointLock, Collector<T> out) throws Exception {
+	public void run(SourceContext<T> ctx) throws Exception {
 		while (isRunning && iterator.hasNext()) {
-			out.collect(iterator.next());
+			ctx.collect(iterator.next());
 		}
 	}
 

flink-staging/flink-streaming/flink-streaming-core/src/test/java/org/apache/flink/streaming/api/complex/ComplexIntegrationTest.java
@@ -484,11 +484,10 @@ public class ComplexIntegrationTest extends StreamingMultipleProgramsTestBase {
 		long cnt = 0;
 
 		@Override
-		public void run(Object checkpointLock,
-				Collector<OuterPojo> out) throws Exception {
+		public void run(SourceContext<OuterPojo> ctx) throws Exception {
 			for (int i = 0; i < 20; i++) {
 				OuterPojo result = new OuterPojo(new InnerPojo(cnt / 2, "water_melon-b"), 2L);
-				out.collect(result);
+				ctx.collect(result);
 			}
 		}
 
@@ -502,11 +501,10 @@ public class ComplexIntegrationTest extends StreamingMultipleProgramsTestBase {
 		private static final long serialVersionUID = 1L;
 
 		@Override
-		public void run(Object checkpointLock,
-				Collector<Tuple2<Long, Tuple2<String, Long>>> out) throws Exception {
+		public void run(SourceContext<Tuple2<Long, Tuple2<String, Long>>> ctx) throws Exception {
 			for (int i = 0; i < 20; i++) {
 				Tuple2<Long, Tuple2<String, Long>> result = new Tuple2<Long, Tuple2<String, Long>>(1L, new Tuple2<String, Long>("a", 1L));
-				out.collect(result);
+				ctx.collect(result);
 			}
 		}
 
@@ -622,10 +620,9 @@ public class ComplexIntegrationTest extends StreamingMultipleProgramsTestBase {
 		}
 
 		@Override
-		public void run(Object checkpointLock,
-				Collector<Rectangle> out) throws Exception {
+		public void run(SourceContext<Rectangle> ctx) throws Exception {
 			for (int i = 0; i < 100; i++) {
-				out.collect(rectangle);
+				ctx.collect(rectangle);
 				rectangle = rectangle.next();
 			}
 		}

flink-staging/flink-streaming/flink-streaming-examples/src/main/java/org/apache/flink/streaming/examples/windowing/TopSpeedWindowingExample.java
@@ -26,7 +26,6 @@ import org.apache.flink.streaming.api.windowing.deltafunction.DeltaFunction;
 import org.apache.flink.streaming.api.windowing.helper.Delta;
 import org.apache.flink.streaming.api.windowing.helper.Time;
 import org.apache.flink.streaming.api.windowing.helper.Timestamp;
-import org.apache.flink.util.Collector;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -108,8 +107,7 @@ public class TopSpeedWindowingExample {
 		}
 
 		@Override
-		public void run(Object checkpointLock, Collector<Tuple4<Integer, Integer, Double, Long>> collector)
-				throws Exception {
+		public void run(SourceContext<Tuple4<Integer, Integer, Double, Long>> ctx) throws Exception {
 
 			while (isRunning) {
 				Thread.sleep(1000);
@@ -122,7 +120,7 @@ public class TopSpeedWindowingExample {
 					distances[carId] += speeds[carId] / 3.6d;
 					Tuple4<Integer, Integer, Double, Long> record = new Tuple4<Integer, Integer, Double, Long>(carId,
 							speeds[carId], distances[carId], System.currentTimeMillis());
-					collector.collect(record);
+					ctx.collect(record);
 				}
 			}
 		}

flink-tests/src/test/java/org/apache/flink/test/checkpointing/StreamCheckpointingITCase.java
@@ -35,7 +35,6 @@ import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
 import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
 import org.apache.flink.test.util.ForkableFlinkMiniCluster;
 
-import org.apache.flink.util.Collector;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -219,7 +218,9 @@ public class StreamCheckpointingITCase {
 		}
 
 		@Override
-		public void run(Object lockingObject, Collector out) throws Exception {
+		public void run(SourceContext<String> ctx) throws Exception {
+			final Object lockingObject = ctx.getCheckpointLock();
+
 			while (isRunning && index < numElements) {
 				char first = (char) ((index % 40) + 40);
 
@@ -230,7 +231,7 @@ public class StreamCheckpointingITCase {
 
 				synchronized (lockingObject) {
 					index += step;
-					out.collect(result);
+					ctx.collect(result);
 				}
 			}
 		}