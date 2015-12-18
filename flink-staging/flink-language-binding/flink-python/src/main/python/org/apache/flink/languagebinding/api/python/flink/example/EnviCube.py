d1272dfc12ecf3b771a62c42a388d2d5b049422e
Loosefish <henrydettmer@gmail.com>
2015-12-18 16:30:14 +0100

    Convert tiles to tuples, group and create cube

--- flink-staging/flink-language-binding/flink-python/src/main/python/org/apache/flink/languagebinding/api/python/flink/example/EnviCube.py
+++ flink-staging/flink-language-binding/flink-python/src/main/python/org/apache/flink/languagebinding/api/python/flink/example/EnviCube.py
@@ -20,25 +20,26 @@ from collections import defaultdict
 from struct import pack
 
 from flink.plan.Environment import get_environment
-from flink.plan.Constants import TILE, STRING
-from flink.plan.Constants import Tile
+from flink.plan.Constants import TILE
 from flink.functions.FlatMapFunction import FlatMapFunction
 from flink.functions.GroupReduceFunction import GroupReduceFunction
-from flink.functions.KeySelectorFunction import KeySelectorFunction
+
+from flink.example.ImageWrapper import tile_to_tuple, tuple_to_tile, ImageWrapper, IMAGE_TUPLE
 
 
 NOVAL = pack("<h", -9999)
 
 
-#deprecated
-class Tokenizer(FlatMapFunction):
+class ToTuple(FlatMapFunction):
     def flat_map(self, value, collector):
-        print("acquisition date: " + value.aquisitionDate + " " + value.pathRow)
-        print("band " + str(value.band) + " lu (" + str(value.leftUpperLon) + ", " + str(value.leftUpperLat) + ")")
-        print(" rl (" + str(value.rightLowerLon) + ", " + str(value.rightLowerLat) + ")")
-        print("pixelsize: " + str(value.xPixelWidth) + " x " + str(value.yPixelWidth))
-        print("tile size: " + str(value.width) + " x " + str(value.height))
-        collector.collect(value)
+        as_tuple = tile_to_tuple(value)
+        collector.collect(as_tuple)
+
+
+class ToTile(FlatMapFunction):
+    def flat_map(self, value, collector):
+        as_tile = tuple_to_tile(value)
+        collector.collect(as_tile)
 
 
 class CubeCreator(GroupReduceFunction):
@@ -49,24 +50,17 @@ class CubeCreator(GroupReduceFunction):
         self.xSize = xSize
         self.ySize = ySize
 
-    @property
-    def leftUpper(self):
-        return (self.leftUpperLat, self.leftUpperLon)
-
-    @property
-    def rightLower(self):
-        return (self.rightLowerLat, self.rightLowerLon)
-
     def reduce(self, iterator, collector):
         """
         :param iterator:
         :param collector:
         :return:
         """
-        # group tiles by band
+        # group images by band
         band_to_tiles = defaultdict(set)
-        for tile in iterator:
-            band_to_tiles[tile._band].add(tile)
+        for image in iterator:
+            image = ImageWrapper(image)
+            band_to_tiles[image.get_meta("band")].add(image)
 
         # iterate over bands in order
         bands = sorted(band_to_tiles.keys())
@@ -74,31 +68,27 @@ class CubeCreator(GroupReduceFunction):
         inside_counter = 0
         known_counter = 0
         for b in bands:
-            result = Tile()
+            sample = band_to_tiles[b].pop()
+            result = ImageWrapper.fromData(sample.coordinates, self.xSize, self.ySize, b,
+                                           sample.get_meta("pathRow"), sample.acquisitionDate,
+                                           sample.get_meta("xPixelWidth"), sample.get_meta("yPixelWidth"))
+            band_to_tiles[b].add(sample)
             # Initialize content with -9999
-            result._content = bytearray(self.xSize * self.ySize * 2)
-            for i in range(0, len(result._content), 2):
-                result._content[i] = NOVAL[0]
-                result._content[i+1] = NOVAL[1]
+            for i in range(0, len(result.content), 2):
+                result.content[i] = NOVAL[0]
+                result.content[i+1] = NOVAL[1]
 
             # iterate over tiles for current band
-            updated = False
             for t in band_to_tiles[b]:
-                if not updated:
-                    result.update(self.leftUpper, self.rightLower, self.xSize,
-                                  self.ySize, b, t._pathRow, t._aquisitionDate,
-                                  t._xPixelWidth, t._yPixelWidth)
-                    updated = True
-
                 for i, (px_coord_lat, px_coord_lon) in coord_iter(t):
-                    if t._content[i:i+2] != NOVAL:
+                    if t.content[i:i+2] != NOVAL:
                         orig_not_null_counter += 1
 
                     if (self.leftUpperLat >= px_coord_lat and
                             px_coord_lat >= self.rightLowerLat and
                             self.leftUpperLon <= px_coord_lon and
                             px_coord_lon <= self.rightLowerLon):
-                        # get index in result tile for current pixel
+                        # get index in result image for current pixel
                         index = int(result.get_content_index_from_coordinate((px_coord_lat, px_coord_lon)))
                         if index >= 0 and index < len(result._content):
                             inside_counter += 1
@@ -108,57 +98,57 @@ class CubeCreator(GroupReduceFunction):
                             result._content[index] = px_value[0]
                             result._content[index+1] = px_value[1]
 
-                collector.collect(result)
+            collector.collect(result._tup)
 
         print("inside", inside_counter)
         print("known_counter", known_counter)
         print("orig not null", orig_not_null_counter)
 
 
-class AcqDateSelector(KeySelectorFunction):
-    def get_key(self, value):
-        return value._aquisitionDate
+def coord_iter(image):
+    lon = image.coordinates[0]
+    lat = image.coordinates[2]
 
+    width = image.get_meta("width")
+    xPixelWidth = image.get_meta("xPixelWidth")
+    yPixelWidth = image.get_meta("yPixelWidth")
 
-def coord_iter(tile):
-    lon = tile._leftUpperLon
-    lat = tile._leftUpperLat
     yield (0, (lat, lon))
-    if len(tile._content) > 2:
-        for i in range(2, len(tile._content), 2):
-            if i % tile._width == 0:
-                lon = tile._leftUpperLon
-                lat -= tile._yPixelWidth
+    if len(image.content) > 2:
+        for i in range(2, len(image.content), 2):
+            if i % width == 0:
+                lon = image.coordinates[0]
+                lat -= yPixelWidth
             else:
-                lon += tile._xPixelWidth
+                lon += xPixelWidth
             yield (i, (lat, lon))
 
 
 if __name__ == "__main__":
-    print("found args length:", len(sys.argv))
-    env = get_environment()
     if len(sys.argv) != 8:
         print("Usage: ./bin/pyflink.sh EnviCube - <dop> <input directory> <left-upper-longitude> <left-upper-latitude> <block size> <pixel size> <output path>")
         sys.exit()
+    env = get_environment()
 
+    # prepare arguments
     dop = int(sys.argv[1])
     path = sys.argv[2]
-    leftLong = sys.argv[3]
-    leftLat = sys.argv[4]
-    blockSize = sys.argv[5]
-    pixelSize = sys.argv[6]
+    leftLon = float(sys.argv[3])
+    leftLat = float(sys.argv[4])
+    blockSize = int(sys.argv[5])
+    pixelSize = int(sys.argv[6])
     outputPath = sys.argv[7]
 
-    leftUpper = (float(leftLat), float(leftLong))
-    rightLower = (float(leftLat) - int(blockSize) * int(pixelSize),
-                  float(leftLong) + int(blockSize) * int(pixelSize))
+    leftUpper = leftLat, leftLon
+    rightLower = leftLat - blockSize * pixelSize, leftLon + blockSize * pixelSize
 
-    data = env.read_envi(path, leftLong, leftLat, blockSize, pixelSize)
-    cube = data.group_by(AcqDateSelector(), STRING)\
-        .reduce_group(CubeCreator(leftUpper, rightLower, int(blockSize), int(blockSize)), TILE)
+    data = env.read_envi(path, str(leftLon), str(leftLat), str(blockSize), str(pixelSize))
 
-    cube.write_envi(outputPath)
+    tuples = data.flat_map(ToTuple(), IMAGE_TUPLE)
+    cube = tuples.group_by(0)\
+                 .reduce_group(CubeCreator(leftUpper, rightLower, blockSize, blockSize), IMAGE_TUPLE)
+    tiles = cube.flat_map(ToTile(), TILE)
+    tiles.write_envi(outputPath)
 
     env.set_degree_of_parallelism(dop)
-
     env.execute(local=True)
---
+++ flink-staging/flink-language-binding/flink-python/src/main/python/org/apache/flink/languagebinding/api/python/flink/example/ImageWrapper.py
@@ -0,0 +1,135 @@
+################################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+import marshal
+from flink.plan.Constants import Tile, STRING, BYTES
+
+
+IMAGE_TUPLE = STRING, BYTES, BYTES
+
+
+def tile_to_tuple(as_tile):
+    tile_meta = {
+        "acquisitionDate": as_tile._aquisitionDate,
+        "coordinates": (as_tile._leftUpperLat, as_tile._rightLowerLat, as_tile._leftUpperLon, as_tile._rightLowerLon),
+        "width": as_tile._width,
+        "height": as_tile._height,
+        "band": as_tile._band,
+        "pathRow": as_tile._pathRow,
+        "xPixelWidth": as_tile._xPixelWidth,
+        "yPixelWidth": as_tile._yPixelWidth
+    }
+
+    return (
+        as_tile._aquisitionDate,
+        bytearray(marshal.dumps(tile_meta)),
+        as_tile._content
+    )
+
+
+def tuple_to_tile(as_tuple):
+    image = ImageWrapper(as_tuple)
+
+    as_tile = Tile()
+    as_tile._aquisitionDate = image.acquisitionDate
+    as_tile._pathRow = image.get_meta('pathRow')
+    as_tile._leftUpperLat = image.coordinates[0]
+    as_tile._leftUpperLon = image.coordinates[2]
+    as_tile._rightLowerLat = image.coordinates[1]
+    as_tile._rightLowerLon = image.coordinates[3]
+    as_tile._width = image.get_meta('width')
+    as_tile._height = image.get_meta('height')
+    as_tile._band = image.get_meta('band')
+    as_tile._xPixelWidth = image.get_meta('xPixelWidth')
+    as_tile._yPixelWidth = image.get_meta('yPixelWidth')
+    as_tile._content = image.content
+
+    return as_tile
+
+
+class ImageWrapper(object):
+    def __init__(self, tup):
+        self._tup = tup
+        self._meta = marshal.loads(tup[1])
+
+    @staticmethod
+    def fromData(coordinates, width, height, band, pathRow,
+                 acquisitionDate, xPixelWidth, yPixelWidth):
+        tile_meta = {
+            "acquisitionDate": acquisitionDate,
+            "coordinates": coordinates,
+            "width": width,
+            "height": height,
+            "band": band,
+            "pathRow": pathRow,
+            "xPixelWidth": xPixelWidth,
+            "yPixelWidth": yPixelWidth
+        }
+
+        return ImageWrapper((
+            acquisitionDate,
+            bytearray(marshal.dumps(tile_meta)),
+            bytearray(width * height * 2)
+        ))
+
+    def get_meta(self, name):
+        return self._meta[name]
+
+    def set_meta(self, name, value):
+        self._meta[name] = value
+        self._tup = (self._tup[0], bytearray(marshal.dumps(self._meta)), self._tup[2])
+
+    @property
+    def acquisitionDate(self):
+        return self._meta["acquisitionDate"]
+
+    @acquisitionDate.setter
+    def acquisitionDate(self, value):
+        self.set_meta("acquisitionDate", value)
+
+    @property
+    def coordinates(self):
+        return self._meta["coordinates"]
+
+    @coordinates.setter
+    def coordinates(self, value):
+        self.set_meta("coordinates", value)
+
+    @property
+    def content(self):
+        return self._tup[2]
+
+    def get_coordinate(self, index):
+        index = index // 2
+        x = index % self.get_meta("width")
+        y = index // self.get_meta("width")
+        newLon = self.coordinates[2] + self.get_meta("xPixelWidth") * x
+        newLat = self.coordinates[0] - self.get_meta("yPixelWidth") * y
+        return (newLat, newLon)
+
+    def get_content_index_from_coordinate(self, coord):
+        lat, lon = coord
+        latDiff = int(self.coordinates[2] - lat)
+        lonDiff = int(lon - self.coordinates[0])
+
+        if latDiff < 0 or lonDiff < 0:
+            return -1
+
+        x = lonDiff // self.get_meta("xPixelWidth")
+        y = latDiff // self.get_meta("yPixelWidth")
+
+        return int(2 * (y * self.get_meta("width") + x))
